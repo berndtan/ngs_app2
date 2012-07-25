1de9398764927954ad0b2c39ad8618386dcbf15f
Annerose <anb128@pitt.edu>
2012-07-25 16:20:13 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NgsApp2::Application.config.secret_token = 'd9c351d613dd08e9db7eb68e83a393bcfc706af81d1f7f6cc10f106a10b24b9bbf435384cafe9d7b6a80aa2caa9cdbc0feeeecbf58aae1a9e25130169e7703d4'
